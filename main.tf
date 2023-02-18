8f9f40cf77efceb6a5403bfb56ec08d704c31baf
faridlamaul <lamaulfarid71@gmail.com>
2023-02-18 22:59:17 +0700

    update main.tf

--- main.tf
+++ main.tf
@@ -1,100 +1,81 @@
-# Configure the AWS Provider with the ap-southeast-1 region
-provider "aws" {
-  region = "ap-southeast-1"
+# Create random_id for suffix name of resources
+resource "random_id" "random_suffix" {
+  byte_length = 8
 }
 
-# Create a VPC
-resource "aws_vpc" "app-vpc" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "app-vpc"
-  }
+# Create s3 bucket for terraform state
+resource "aws_s3_bucket" "terraform-state" {
+  bucket        = "terraform-state-${random_id.random_suffix.hex}"
+  force_destroy = true
 }
 
-# Create a public subnet
-resource "aws_subnet" "app-public-subnet" {
-  vpc_id     = aws_vpc.app-vpc.id
-  cidr_block = "10.0.1.0/24"
-
-  tags = {
-    Name = "app-public-subnet"
-  }
+# Create s3 bucket acl for terraform state
+resource "aws_s3_bucket_acl" "terraform-state-acl" {
+  bucket = aws_s3_bucket.terraform-state.id
+  acl    = "private"
 }
 
-# Create an Internet Gateway
-resource "aws_internet_gateway" "app-igw" {
-  vpc_id = aws_vpc.app-vpc.id
-
-  tags = {
-    Name = "app-igw"
+# Create s3 bucket versioning for terraform state
+resource "aws_s3_bucket_versioning" "terraform-state-versioning" {
+  bucket = aws_s3_bucket.terraform-state.id
+  versioning_configuration {
+    status = "Enabled"
   }
 }
 
-# Create a security group
-resource "aws_security_group" "app-sg" {
-  name        = "app-sg"
-  description = "Allow HTTP and SSH traffic"
-  vpc_id      = aws_vpc.app-vpc.id
-
-  # Allow SSH Traffic
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Allow HTTP Traffic
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Allow HTTPS Traffic
-  ingress {
-    description = "HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+# Create dynamodb table for terraform state
+resource "aws_dynamodb_table" "terraform-state-lock" {
+  name         = "terraform-state-lock-${random_id.random_suffix.hex}"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
 
-# Create instance
-resource "aws_instance" "app-instance" {
-  # AMI for CentOS 7
-  ami                    = "ami-0a8ab25085477038e"
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.app-public-subnet.id
-  vpc_security_group_ids = aws_security_group.app-sg.id
-
-  tags = {
-    Name = "app-instance"
-  }
-
-  # Provisioner to install nginx and nodejs
-  provisioner "remote-exec" {
-    inline = [
-      "yum update -y",
-      "yum install -y epel-release",
-      "yum install -y nodejs",
-      "yum install -y nginx",
-      "systemctl start nginx",
-      "systemctl enable nginx",
-      "cd /usr/share/nginx/html",
-      "echo '<h1>Node.js App</h1>' > index.html",
-      "cd /etc/nginx/conf.d/",
-      "echo 'server { \n listen 80;\n server_name _;\n return 301 https://$host$request_uri;\n}' > redirect_to_https.conf",
-      "systemctl restart nginx",
-    ]
-  }
+# Create EC2 Instance 1
+resource "aws_instance" "ec2_instance_1" {
+  ami             = "ami-0a8ab25085477038e" # AMI for CentOS 7
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.sg_instances.name]
+  # Install nginx and nodejs
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              curl -sL https://rpm.nodesource.com/setup_12.x | sudo bash -
+              yum install -y nodejs
+              yum install -y nginx
+              systemctl start nginx
+              systemctl enable nginx
+              cd /usr/share/nginx/html
+              echo '<h1>Node.js App 2</h1>' > index.html
+              cd /etc/nginx/conf.d/
+              echo 'server { \n listen 80;\n server_name _;\n return 301 https://$host$request_uri;\n}' > redirect_to_https.conf
+              systemctl restart nginx
+              python3 -m http.server 8080 &
+              EOF
 }
 
-output "nodejs_instance_public_ip" {
-  value = aws_instance.app-instance.public_ip
+# Create EC2 Instance 2
+resource "aws_instance" "ec2_instance_2" {
+  ami             = "ami-0a8ab25085477038e" # AMI for CentOS 7
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.sg_instances.name]
+  # Install nginx and nodejs
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              curl -sL https://rpm.nodesource.com/setup_12.x | sudo bash -
+              yum install -y nodejs
+              yum install -y nginx
+              systemctl start nginx
+              systemctl enable nginx
+              cd /usr/share/nginx/html
+              echo '<h1>Node.js App 2</h1>' > index.html
+              cd /etc/nginx/conf.d/
+              echo 'server { \n listen 80;\n server_name _;\n return 301 https://$host$request_uri;\n}' > redirect_to_https.conf
+              systemctl restart nginx
+              python3 -m http.server 8080 &
+              EOF
 }
